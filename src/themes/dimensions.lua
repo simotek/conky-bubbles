c57f9491f38df583bf4c418dcfdb24b7538a3e8f
Simon Lees <sflees@suse.de>
2025-09-22 11:35:41 +0930

    Dimensions: Massive update to this theme

--- src/themes/dimensions.lua
+++ src/themes/dimensions.lua
@@ -3,11 +3,24 @@
 -- @alias dt
 
 pcall(function() require('cairo') end)
+local ch = require('src/cairo_helpers')
 
 -- Specified here so it can be used in multiple places
 local highlight_color = "3498d8"
+local temperature_colors = {
+        highlight_color,
+        "025286",
+        "484b9a",
+        "8e44ad",
+        "8d3733",
+        "8d2f25",
+    }
+-- This is different in other themes
+local header_color = highlight_color
 
-theme = {
+local header_font_size = 16
+
+local theme = {
     --- Font used by widgets if no other is specified.
     -- @string default_font_family
     default_font_family = "SUSE",
@@ -22,7 +35,7 @@ theme = {
 
     --- Font size used by widgets if no other is specified.
     -- @int default_font_size
-    header_font_size = 14,
+    header_font_size = header_font_size,
 
     default_font_direction = "LTR", 
     default_font_script = "Zyyy", 
@@ -42,7 +55,7 @@ theme = {
 
     --- Color used for drawing titles / headers (not used by widgets)
     -- @string header_color a color hex string
-    header_color = "025286",
+    header_color = highlight_color,
 
     --- Color used to draw graph widgets if no other is specified.
     -- @string graph_color a color hex string
@@ -66,14 +79,16 @@ theme = {
     --- A table of colors that are used for temperature graphs
     -- in the CPU Widgets
     -- @table strings containing hex colors without the leading #
-    temperature_colors = {
-        highlight_color,
-        "025286",
-        "484b9a",
-        "8e44ad",
-        "8d3733",
-        "8d2f25",
-    },
+    temperature_colors = temperature_colors,
+
+    --- Font used in block widget for header
+    -- @table A table of args that can be passed to Static or Conky text
+    header_font = {color=header_color,font_family="Sixteen", font_size=header_font_size, border_width=0.8, border_color=temperature_colors[2]},
+    
+    --- Font used in block widget for status text
+    -- @table A table of args that can be passed to Static or Conky text
+    status_font = {color=temperature_colors[3],font_family="Sixteen-Mono", font_size=header_font_size, border_width=0.3, border_color=temperature_colors[2]},
+
 
     --- Color for Green LED's.
     -- @string green_led_color a color hex string
